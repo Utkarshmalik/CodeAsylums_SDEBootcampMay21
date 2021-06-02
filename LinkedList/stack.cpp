bef3b57479789fd5778483f2235c3d1352f3e1e5
mohitverma6161 <utkarshmalik06@gmail.com>
2021-06-02 20:06:35 +0530

    stack day 2

--- LinkedList/stack.cpp
+++ LinkedList/stack.cpp
@@ -410,3 +410,83 @@ int main()
 
 
 }
+
+
+
+---------------------------------
+
+
+
+// { Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+        
+        int n=s.size();
+        
+        int l=0,r=0,ans=0;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            l++;
+            else 
+            r++;
+            
+            if(l==r)
+            {
+                ans=max(ans,2*l);
+            }
+
+             else if(r>=l)
+            { 
+                l=0;
+                r=0;
+            }
+        }
+        
+        l=0,r=0;
+        
+          for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]=='(')
+            l++;
+            else 
+            r++;
+            
+            if(l==r)
+            {
+                ans=max(ans,2*l);
+            }
+             else if(l>=r)
+            { 
+                l=0;
+                r=0;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
