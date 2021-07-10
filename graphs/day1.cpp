5500e7ab747bddaca052d9ca0a6848f775fd2b75
mohitverma6161 <utkarshmalik06@gmail.com>
2021-07-10 20:04:26 +0530

    cycle

--- graphs/day1.cpp
+++ graphs/day1.cpp
@@ -1,46 +1,31 @@
-#include<iostream>
-using namespace std;
 
-
-int main()
-{
-    int n;
-    cin>>n;
-
-    int graph[n][n];
-
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            graph[i][j]=0;
-        }
-    }
-
-    int e;
-    cin>>e;
-
-    for(int i=0;i<e;i++)
-    {
-        int src,dest;
-        cin>>src>>dest;
-
-        src--;
-        dest--;
-
-        graph[src][dest]=1;
-        graph[dest][src]=1;
-    }
-
-
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            cout<<graph[i][j]<<" ";
-        }
-
-        cout<<"\n";
-    }
-    
-}
+       vector<int>res;
+	    queue<int>q;
+	   vector<bool>visited(V,false);
+	    
+	    q.push(0);
+	    visited[0]=true;
+	    
+	    while(!q.empty())
+	    {
+	        int node=q.front();
+	        res.push_back(node);
+	        q.pop();
+	        
+	        
+	        int n=adj[node].size();
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            int child=adj[node][i];
+	            
+	            if(!visited[child])
+	            {
+	                visited[child]=true;
+	                q.push(child);
+	            }
+	        }
+	    }
+	    
+	    return res;
+	    
